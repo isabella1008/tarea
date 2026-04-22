6c8223c543a5f1a71bc9cb9a351c53cbdc85dbab
reacertaxs <tpueblarojas@gmail.com>
2026-04-22 14:48:37 -0400

    Refactor main loop for concert ticket sales system

--- README.py
+++ README.py
@@ -6,12 +6,12 @@ dias_2 = []
 dias_3 = []
 tipos_plan = []
 
-opcion = 0
+print("SISTEMA DE PLANIFICACIÓN DE VENTA DE ENTRADAS A CONCIERTOS")
 
-while opcion != 5:
-    print("----SISTEMA DE PLANIFICACIÓN DE VENTA DE ENTRADAS A CONCIERTOS----")
-    print("1. Generar plan de ventas ")
-    print("2. Ingresar entradas vendidas ")
-    print("3. Visualizar ingreso total por concierto ")
-    print("4. Visualizar ingreso total por día ")
-    print("5. Salir")
+while True:
+    print("----------")
+    try:
+        accion = int(input("\n1.Generar plan de ventas \n2. Ingresar entradas vendidas \n3. Visualizar ingreso total por concierto \n4. Visualizar ingreso total por día \n5. Salir del programa \n---------- \nAccion:").strip())
+    except:
+        print("Error, debe ingresar un numero valido")
+    
