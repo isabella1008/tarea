896e6da4bb31692aadce69a2969c0fee5e768385
Matikjs12 <matiasvlnz12@gmail.com>
2026-04-22 14:46:33 -0400

    Implement main menu for user interaction
    Added a menu loop for user options in the program.

--- README.py
+++ README.py
@@ -1,2 +1,16 @@
 # tarea
-print("El colo el más grande de Chile")
+Concierto_1 = []
+Concierto_2 = []
+dias_1 = []
+dias_2 = []
+dias_3 = []
+tipos_plan = []
+
+opcion = 0
+
+while opcion != 4:
+    print("\n--- MENÚ PRINCIPAL ---")
+    print("1. Registrar inscripción")
+    print("2. Reporte")
+    print("3. Total ingresos")
+    print("4. Salir")
