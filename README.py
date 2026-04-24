a33e7522a460bbb0f95bc80a2aff08a677023553
reacertaxs <tpueblarojas@gmail.com>
2026-04-23 23:22:03 -0400

    Refactor action handling in README.py

--- README.py
+++ README.py
@@ -42,13 +42,36 @@ while True:
             print()
 
         print("*" * 40)
-    if accion == 2:
+    elif accion == 2:
         pass
-    if accion == 3:
+    elif accion == 3:
         pass
-    if accion == 4:
-        pass
-    if accion == 5:
-        pass
+    elif accion == 4:
+        try:
+            dia_calcular = int(input("ingrese el dia a visualizar y calcular: ").strip())
+        except:
+            print("Error, debe ingresar un numero valido")
+        if dia_calcular == 1:
+            print("concierto 1, dia 1: ", monto_total11) #muestra el total del concierto 1, dia 1
+            print("concierto 2, dia 1: ", monto_total12) #muestra el total del concierto 2, dia 1
+            total_dia1 = monto_total11 + monto_total12 #se suman las ganancias totales del dia 1
+            print("ingreso total dia 1: ", total_dia1) #se le muestra al usuario las ganancias totales del dia 1
+        elif dia_calcular == 2: 
+            print("concierto 1, dia 2: ", monto_total21) #muestra el total del concierto 1, dia 2
+            print("concierto 2, dia 2: ", monto_total22) #muestra el total del concierto 2, dia 2
+            total_dia2 = monto_total21 + monto_total22 #se suman las ganancias totales del dia 2
+            print("ingreso total dia 1: ", total_dia2) #se le muestra al usuario las ganancias totales del dia 2
+        elif dia_calcular == 3:
+            print("concierto 1, dia 3: ", monto_total31) #muestra el total del concierto 1, dia 3
+            print("concierto 2, dia 3: ", monto_total32) #muestra el total del concierto 2, dia 3
+            total_dia3 = monto_total31 + monto_total32 #se suman las ganancias totales del dia 3
+            print("ingreso total dia 1: ", total_dia3) #se le muestra al usuario las ganancias totales del dia 3
+        else:
+            print("ERROR, porfavor seleccione solo 1 de los dias disponibles (1, 2, 3)") #error si el usuario ingresa calquier cosa 
+    elif accion == 5: #termino del programa
+        print("hasta luego👋")
+        input("presione enter para cerrar...")
+        break #termina el while
     else:
-        break
+        print("ERROR, porfavor seleccione 1 de las opciones disponibles (1, 2, 3, 4 o 5)") #error si el usuario no ingresa ninguna opcion
+    
