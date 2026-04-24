60c171128a5f0d08269fac456210b651899a5097
Isidora-oc <isidorabelenoleacid@gmail.com>
2026-04-23 21:30:23 -0400

    Implement sales plan generation for concerts
    Added functionality to create a sales plan for concerts with a 2D array for sales tracking.

--- README.py
+++ README.py
@@ -5,6 +5,7 @@ dias_1 = []
 dias_2 = []
 dias_3 = []
 tipos_plan = []
+plan_ventas = []
 
 print("SISTEMA DE PLANIFICACIÓN DE VENTA DE ENTRADAS A CONCIERTOS")
 
@@ -14,8 +15,33 @@ while True:
     except:
         print("Error, debe ingresar un numero valido")
     if accion == 1:
-        ingreso_concierto = int(input("Ingrese número de conciertos:"))
-        ingreso_dia = int(input("Ingrese número de días de ventas:"))
+        conciertos = int(input("Ingrese número de conciertos: "))  # filas
+        dias = int(input("Ingrese número de días de ventas: "))    # columnas
+
+        for i in range(conciertos):  # i = concierto (fila)
+            fila = []
+            for j in range(dias):  # j = día (columna)
+                fila.append(0)  # parte en 0 porque no hay ventas
+            plan_ventas.append(fila)
+
+        # mostrar tabla
+        print("\nPlan de ventas")
+        print("*" * 40)
+
+        # días arriba
+        print(" " * 15, end="")
+        for d in range(dias):
+            print(f"{d+1:>5}", end="")
+        print()
+
+        # mostrar conciertos y datos
+        for i in range(conciertos):  # recorro cada fila
+            print(f"Concierto {i+1:<5}", end="")
+            for j in range(dias):  # recorro cada columna
+                print(f"{plan_ventas[i][j]:>5}", end="")  # valor en fila i, columna j
+            print()
+
+        print("*" * 40)
     if accion == 2:
         pass
     if accion == 3:
