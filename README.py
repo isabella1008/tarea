12166063bc95000a2651acf1874dbcec7b608851
reacertaxs <tpueblarojas@gmail.com>
2026-04-23 23:24:48 -0400

    Update README.py

--- README.py
+++ README.py
@@ -11,7 +11,7 @@ print("SISTEMA DE PLANIFICACIÓN DE VENTA DE ENTRADAS A CONCIERTOS")
 
 while True:
     try:
-        accion = int(input("---------- \n1.Generar plan de ventas \n2. Ingresar entradas vendidas \n3. Visualizar ingreso total por concierto \n4. Visualizar ingreso total por día \n5. Salir del programa \n---------- \nAccion: ").strip())
+        accion = int(input("---------- \n1. Generar plan de ventas \n2. Ingresar entradas vendidas \n3. Visualizar ingreso total por concierto \n4. Visualizar ingreso total por día \n5. Salir del programa \n---------- \nAccion: ").strip())
     except:
         print("Error, debe ingresar un numero valido")
     if accion == 1:
